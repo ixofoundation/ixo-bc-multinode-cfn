d59d0dca07c702c1128f9b0a7159a03dc0987a97
source-code-smith <herman@sourcecodesmith.com>
2018-11-21 16:03:50 +0200

    performing actual variable replacement via sed and then reverting changes post create command

--- scripts/create-blockchain-stack.sh
+++ scripts/create-blockchain-stack.sh
@@ -12,10 +12,10 @@ echo "NETWORK_STACK: $NETWORK_STACK"
 echo "KEY_NAME: $KEY_NAME"
 
 sed -i '' "s|%%TargetEnvironment%%|$TARGET_ENVIRONMENT|" "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
-# sed -i '' "s|%%ImageTag%%|$IMAGE_TAG|" "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
-# sed -i '' "s|%%NetworkStackName%%|$NETWORK_STACK|" "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
-# sed -i '' "s|%%KeyName%%|$KEY_NAME|" "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
-#
-# aws cloudformation create-stack --stack-name Blockchain-Stack$STACK_SUFFIX --template-body file://templates/BlockchainStack.yaml  --parameters file://parameters/BlockchainStack.parameters.json --profile default --region $TARGET_REGION
-#
-# git checkout "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
+sed -i '' "s|%%ImageTag%%|$IMAGE_TAG|" "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
+sed -i '' "s|%%NetworkStackName%%|$NETWORK_STACK|" "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
+sed -i '' "s|%%KeyName%%|$KEY_NAME|" "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
+
+aws cloudformation create-stack --stack-name Blockchain-Stack$STACK_SUFFIX --template-body file://templates/BlockchainStack.yaml  --parameters file://parameters/BlockchainStack.parameters.json --profile default --region $TARGET_REGION
+
+git checkout "$SCRIPTS_DIR/../parameters/BlockchainStack.parameters.json"
